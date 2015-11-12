802f13e25f90a7d0a05212c6d4bcb8c49e29a61c
karimserhan <kfserhan@gmail.com>
2015-11-12 17:25:44 -0600

    changes

--- nose.sh
+++ nose.sh
@@ -3,4 +3,4 @@
 PYTHONPATH=''
 nosetests --with-xunit --all-modules --traverse-namespace --with-coverage --cover-package=./ --cover-inclusive
 python -m coverage xml --include=./*
-pylint -f parseable -d I0011,R0801 *.py | tee pylint.out
+#pylint -f parseable -d I0011,R0801 *.py | tee pylint.out
